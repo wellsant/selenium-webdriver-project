88abdb823123de66aa6b86b85fdd0e7c7d92ae94
Wellington Santana <wellsant0606@gmail.com>
2023-09-06 06:09:04 -0300

    Refactor 3

---
+++ test_comandos_asserts.py
@@ -0,0 +1,17 @@
+# O assert sempre verifica se o retorno da condição é true
+
+assert True
+
+# assert numbers
+
+num_esperado = 2
+num_obtido = 1
+assert  num_esperado > num_obtido, f"Esperado '{num_esperado}'. Atual '{num_obtido}'"
+
+
+# assert text
+
+# assert text in string
+
+# assert is_displeyed/is_enbled/is_selected
+
